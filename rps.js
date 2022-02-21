00ad6e304e5813b3aab475d94bf28f2fa35d8676
Aste <aste@tuta.io>
2022-02-21 21:54:31 +0100

    added 3 functions to play 5 rounds of RPS against the computer

--- rps.js
+++ rps.js
@@ -1,1 +1,41 @@
-console.log("Hello World");
+
+// console.log("Hello World");
+// generate a random computer choice for Rock Paper Sissors
+const computerPlay = () => {
+    let comChoice = Math.floor(Math.random() * 3);
+    console.log(comChoice);
+    if (comChoice === 0) {
+        // console.log('The computerPlay was rock');
+        return 'rock';
+    } else if (comChoice === 1) {
+        // console.log('The computerPlay was paper');
+        return 'paper';
+    } else {
+        // console.log('The computerPlay was sissors');
+        return 'sissors'; 
+    };
+};
+
+// Compares the computer play to the user play and returns a string delcaring the winner
+const playRound = (playerSelection, computerSelection) => {
+    computerSelection = computerPlay();
+    playerSelection = (window.prompt('Please chooce; rock, paper or sissors', 'rock')).toLowerCase();
+    if (playerSelection === computerSelection) {
+        console.log('This round was a draw');
+        return 'This round was a draw';
+    } else if ((playerSelection === 'rock' && computerSelection === 'paper') || (playerSelection === 'paper' && computerSelection === 'sissors') || (playerSelection === 'sissors' && computerSelection === 'rock')) {
+        console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
+        return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    } else {
+        console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
+        return `You Win! ${playerSelection} beats ${computerSelection}`;
+    }
+};
+
+const game = () => {
+    for (let i = 0; i < 5; i++){
+        playRound()
+    }
+};
+
+game()
