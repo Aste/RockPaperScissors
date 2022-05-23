2d1921671d005ff3f6cba8336bfc17b494386536
Aste <aste@tuta.io>
2022-05-23 16:55:14 +0200

    Add functional Rock Paper Sciccors buttons

--- rps.js
+++ rps.js
@@ -1,6 +1,6 @@
 
 // console.log("Hello World");
-// generate a random computer choice for Rock Paper Sissors
+// generate a random computer choice for Rock Paper Scissors
 const computerPlay = () => {
     let comChoice = Math.floor(Math.random() * 3);
     console.log(comChoice);
@@ -11,19 +11,19 @@ const computerPlay = () => {
         // console.log('The computerPlay was paper');
         return 'paper';
     } else {
-        // console.log('The computerPlay was sissors');
-        return 'sissors'; 
+        // console.log('The computerPlay was scissors');
+        return 'scissors'; 
     };
 };
 
 // Compares the computer play to the user play and returns a string delcaring the winner
 const playRound = (playerSelection, computerSelection) => {
     computerSelection = computerPlay();
-    playerSelection = (window.prompt('Please chooce; rock, paper or sissors', 'rock')).toLowerCase();
+    // playerSelection = (window.prompt('Please chooce; rock, paper or scissors', 'rock')).toLowerCase();
     if (playerSelection === computerSelection) {
         console.log('This round was a draw');
         return 'This round was a draw';
-    } else if ((playerSelection === 'rock' && computerSelection === 'paper') || (playerSelection === 'paper' && computerSelection === 'sissors') || (playerSelection === 'sissors' && computerSelection === 'rock')) {
+    } else if ((playerSelection === 'rock' && computerSelection === 'paper') || (playerSelection === 'paper' && computerSelection === 'scissors') || (playerSelection === 'scissors' && computerSelection === 'rock')) {
         console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
         return `You Lose! ${computerSelection} beats ${playerSelection}`;
     } else {
@@ -32,10 +32,29 @@ const playRound = (playerSelection, computerSelection) => {
     }
 };
 
-const game = () => {
-    for (let i = 0; i < 5; i++){
-        playRound()
-    }
-};
 
-game()
+const btnRock = document.querySelector('#btnRock');
+    btnRock.addEventListener('click', () => {
+        playRound('rock');
+});
+
+const btnPaper = document.querySelector('#btnPaper');
+    btnPaper.addEventListener('click', () => {
+        playRound('paper');
+});
+
+const btnSciccors = document.querySelector('#btnSciccors');
+    btnSciccors.addEventListener('click', () => {
+        playRound('sciccors');
+});
+
+
+
+
+// const game = () => {
+// // for (let i = 0; i < 5; i++){
+//     //     playRound()
+//     // }
+// };
+// 
+// game()
