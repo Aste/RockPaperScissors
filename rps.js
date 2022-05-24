0c0743f946ec63f2ac50073f73968370e9e42ac5
Aste <aste@tuta.io>
2022-05-24 17:49:06 +0200

    Refactor codebase with automatic restart of game and announcement of winner after 5 points is reached

--- rps.js
+++ rps.js
@@ -1,3 +1,26 @@
+let computerScore = 0;
+let playerScore = 0;
+let playerSelection = null;
+let firstMessage = "Let the game begin, please choose; rock, paper or scissors";
+
+
+// Input buttons for player choice and game initialization
+const btnRock = document.querySelector('#btnRock');
+btnRock.addEventListener('click', () => {
+    playRound('rock');
+})
+
+const btnPaper = document.querySelector('#btnPaper');
+btnPaper.addEventListener('click', () => {
+    playRound('paper');
+});
+
+const btnSciccors = document.querySelector('#btnSciccors');
+btnSciccors.addEventListener('click', () => {
+    playRound('scissors');
+})
+
+
 // Generate a random computer choice for Rock Paper Scissors
 const computerPlay = () => {
     let comChoice = Math.floor(Math.random() * 3);
@@ -11,65 +34,50 @@ const computerPlay = () => {
     } else {
         // console.log('The computerPlay was scissors');
         return 'scissors';
-    };
-};
+    }
+}
 
-
-let computerScore = 0;
-let playerScore = 0;
-let playerSelection = null;
-let firstMessage = "Let the game begin, please choose; rock, paper or scissors";
-
-// Compares the computer play to the user play and returns a string delcaring the winner
 const playRound = (playerSelection, computerSelection) => {
     computerSelection = computerPlay();
     // playerSelection = (window.prompt('Please choose; rock, paper or scissors', 'rock')).toLowerCase();
     if (playerSelection === computerSelection) {
         firstMessage = 'This round was a draw';
         divResult.innerText = `${firstMessage} \n ${computerScore} points : Computer \n ${playerScore} points : You`
-        return;
     } else if ((playerSelection === 'rock' && computerSelection === 'paper') || (playerSelection === 'paper' && computerSelection === 'scissors') || (playerSelection === 'scissors' && computerSelection === 'rock')) {
-        firstMessage = `You lose! ${computerSelection} beats ${playerSelection}`;
         computerScore += 1;
         divResult.innerText = `${firstMessage} \n ${computerScore} points : Computer \n ${playerScore} points : You`
-        return;
+        if (computerScore === 5) {
+            firstMessage = `Game over! The Computer won with 5 points. To start a new game please choose; rock, paper or scissors`;
+            divResult.innerText = firstMessage;
+            console.log(firstMessage);
+            console.log(computerScore);
+            computerScore = 0;
+            playerScore = 0;
+            playerSelection = null;
+        } else {
+        firstMessage = `You lost this round! ${computerSelection} beats ${playerSelection}`;
+
+        }
     } else {
-        firstMessage = `You win! ${playerSelection} beats ${computerSelection}`;
         playerScore += 1;
         divResult.innerText = `${firstMessage} \n ${computerScore} points : Computer \n ${playerScore} points : You`
-        return;
-    }
-};
-
-const btnRock = document.querySelector('#btnRock');
-btnRock.addEventListener('click', () => {
-    playRound('rock');
-});
-
-const btnPaper = document.querySelector('#btnPaper');
-btnPaper.addEventListener('click', () => {
-    playRound('paper');
-});
-
-const btnSciccors = document.querySelector('#btnSciccors');
-btnSciccors.addEventListener('click', () => {
-    playRound('scissors');
-});
-
-const body = document.body
-const divResult = document.createElement("div")
-divResult.innerText = `${firstMessage} \n ${computerScore} points : Commputer \n ${playerScore} points : You`
-body.append(divResult)
-// document.getElementsByTagName("divResult").style.marginTop = "50px";
-
-const game = () => {
-    while (playerSelection === true) {
-        while (playerScore && computerScore > 5) {
-            playRound()
+        if (playerScore === 5) {
+            firstMessage = `Game over! You won with 5 points. To start a new game please choose; rock, paper or scissors`;
+            divResult.innerText = firstMessage;
+            console.log(firstMessage);
+            console.log(computerScore);
+            computerScore = 0;
+            playerScore = 0;
+            playerSelection = null;
+        } else {
+            firstMessage = `You won this round! ${playerSelection} beats ${computerSelection}`;
         }
-        firstMessage = "Game Over";
-        break
     }
-};
+}
 
-game()
+
+// Add scoreboard and game inital messesage
+const body = document.body
+const divResult = document.createElement("div");
+divResult.innerText = (`${firstMessage} \n ${computerScore} points : Computer \n ${playerScore} points : You`);
+body.append(divResult);
